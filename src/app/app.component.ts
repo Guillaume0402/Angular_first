36cf81829f38985da160fa1103e0df41d7125787
Guillaume0402 <168349909+Guillaume0402@users.noreply.github.com>
2024-12-17 23:32:41 +0100

    Initial commit du projet Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
+import { PlayingCardComponent } from './components/playing-card/playing-card.component';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [PlayingCardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'playing-cards';
-}
+export class AppComponent {}
